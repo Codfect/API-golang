fa78680be1bfe2a5ad8f808ac7eeccdd8cfb807a
Oliver <oliverjgodoy@gmail.com>
2020-12-01 16:21:55 -0300

    pass test multiple 3 = "Fizz"

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -1,11 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println("Escre um número inteiro:")
+	var inteiro int
+	fmt.Scanf("%d", &inteiro)
+
+	for i := 1; i <= inteiro; i++ {
+		fmt.Println(fizzbuzz(i))
+	}
 }
 
 func fizzbuzz(n int) string {
 	var resultado string
+	//Fizz = 3 * n
+	if n%3 == 0 {
+		resultado = "Fizz"
+	}
 
 	return resultado
 }
--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -4,6 +4,12 @@ import (
 	"testing"
 )
 
+// A palavra “Fizz” deve ser substituída no lugar de múltiplos do número três
 func TestFizzBuzz(t *testing.T) {
+	param := fizzbuzz(3)
+	fizz := "Fizz"
 
+	if param != fizz {
+		t.Errorf("fizzbuzz3(3) \n param: %v \n fizz: %v", param, fizz)
+	}
 }
